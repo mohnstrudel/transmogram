6ee285cde91fd1b38a3f86967e0508f8e6b89fe8
Anton Kostin <anton.kostin@mhm-hr.com>
2019-03-07 20:14:51 +0100

    TMG-002: Admin and Front namespaces, initial bootstrap and admin theme setup with yarn.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope module: :front do
+    root "static_pages#home"
+  end
+
+  namespace :admin do
+    get '/', to: 'dashboard#index'
+  end
 end
