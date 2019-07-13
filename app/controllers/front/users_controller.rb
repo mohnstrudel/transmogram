fbb38008b270881575a1bf77f10963f2f7db5e88
Anton Kostin <anton.kostin@mhm-hr.com>
2019-07-13 23:17:45 +0200

    TMG-024: Hash Tags on User show page.

--- app/controllers/front/users_controller.rb
+++ app/controllers/front/users_controller.rb
@@ -1,5 +1,7 @@
 class Front::UsersController < FrontController
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(posts: :images).includes(posts: :comments).find(params[:id])
+    @posts = @user.posts
+    @tags = Post.joins(:hash_tags).includes(:hash_tags).where(user: @user).select{|post| p post.hash_tags}.pluck(:title).flatten
   end
 end
